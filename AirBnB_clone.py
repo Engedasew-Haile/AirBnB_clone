3860c02ea186eea44e6b0d78e4ec6cac0a1a1db8
Engedasew-Haile <engedasewhaile@gmail.com>
2023-05-25 08:01:15 -0700

    update

---
+++ AirBnB_clone.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def main():
+    numbers = [2, 4, 6, 8] 
+    product = 1
+    for num in numbers:
+        product = product * num
+    print(product)
+
+if __name__ == "__main__":
+    main()
